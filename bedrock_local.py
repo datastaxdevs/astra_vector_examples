009a701ed59834f32954b4134a92cbcc64848b86
synedra-datastax <synedra@gmail.com>
2023-09-26 11:58:28 -0700

    Adding dotenv

--- bedrock_local.py
+++ bedrock_local.py
@@ -1,8 +1,8 @@
-
+# You need to perform these commands first
 # curl https://d2eo22ngex1n9g.cloudfront.net/Documentation/SDK/bedrock-python-sdk.zip --output bedrock-python-sdk.zip
 # unzip -o bedrock-python-sdk.zip
 # pip install -r requirements_bedrock.txt
-# pip install dotenv
+# pip install dotenv *.whl
 # mkdir texts
 # curl https://raw.githubusercontent.com/synedra/astra_vector_examples/main/romeo_astra.json --output texts/romeo_astra.json
 
@@ -25,8 +25,9 @@ from getpass import getpass
 from langchain.docstore.document import Document
 from langchain.document_loaders import TextLoader
 from dotenv import load_dotenv
+
 load_dotenv()
-  
+
 cluster = Cluster(
     cloud={
         "secure_connect_bundle": os.environ["ASTRA_DB_SECURE_BUNDLE_PATH"],
@@ -38,27 +39,35 @@ cluster = Cluster(
 )
 session = cluster.connect()
 
-bedrock=boto3.client('bedrock','us-west-2',endpoint_url='https://invoke-bedrock.us-west-2.amazonaws.com')
-br_embeddings = BedrockEmbeddings(model_id="amazon.titan-embed-g1-text-02", client=bedrock)
+bedrock = boto3.client(
+    "bedrock",
+    "us-west-2",
+    endpoint_url="https://invoke-bedrock.us-west-2.amazonaws.com",
+)
+br_embeddings = BedrockEmbeddings(
+    model_id="amazon.titan-embed-g1-text-02", client=bedrock
+)
 
 myCassandraVStore = Cassandra(
-        embedding=br_embeddings,
-        session=session,
-        keyspace=os.environ["ASTRA_DB_KEYSPACE"],
-        table_name='shakespeare_act5'
-        )
-quote_array = json.load(open('texts/romeo_astra.json'))\n"
+    embedding=br_embeddings,
+    session=session,
+    keyspace=os.environ["ASTRA_DB_KEYSPACE"],
+    table_name="shakespeare_act5",
+)
+quote_array = json.load(open("texts/romeo_astra.json"))
 for index in range(len(quote_array)):
-        location = ''
-        if (quote_array[index]['ActSceneLine'] != ''):
-           (act, scene, line) = quote_array[index]['ActSceneLine'].split('.')
-           location = 'Act {}, Scene {}, Line {}'.format(act, scene, line)
-        quote_input = '{} : {} : {}'.format(location, quote_array[index]['Player'], quote_array[index]['PlayerLine'])
-        input_document = Document(page_content=quote_input)
-        print(quote_input)
-        myCassandraVStore.add_documents(documents=[input_document])
+    location = ""
+    if quote_array[index]["ActSceneLine"] != "":
+        (act, scene, line) = quote_array[index]["ActSceneLine"].split(".")
+        location = "Act {}, Scene {}, Line {}".format(act, scene, line)
+    quote_input = "{} : {} : {}".format(
+        location, quote_array[index]["Player"], quote_array[index]["PlayerLine"]
+    )
+    input_document = Document(page_content=quote_input)
+    print(quote_input)
+    myCassandraVStore.add_documents(documents=[input_document])
 
-      # Enter a question about Romeo and Astra (Like 'How did Astra die?')
+# Enter a question about Romeo and Astra (Like 'How did Astra die?')
 QUESTION_FOR_MODEL = "How did Astra Die?"
 generation_prompt_template = f"""Please answer a question from a user.
       Create a summary of the information between ## to answer the question.
@@ -71,52 +80,35 @@ generation_prompt_template = f"""Please answer a question from a user.
       question= {question}
       Answer: """
 
-retriever = myCassandraVStore.as_retriever(search_kwargs={'k':2,})
-output=retriever.get_relevant_documents(QUESTION_FOR_MODEL)
+retriever = myCassandraVStore.as_retriever(
+    search_kwargs={
+        "k": 2,
+    }
+)
+output = retriever.get_relevant_documents(QUESTION_FOR_MODEL)
 prompt = PromptTemplate.from_template("{page_content}")
-context = ''
+context = ""
 for document in output:
-    context += ' *** ' + document.page_content 
-        
+    context += " *** " + document.page_content
+
 print(context)
 llm_prompt = generation_prompt_template.format(
-            question=QUESTION_FOR_MODEL,
-            context=context,
-        )
+    question=QUESTION_FOR_MODEL,
+    context=context,
+)
 
-body = json.dumps({'inputText': llm_prompt})
-modelId = 'amazon.titan-tg1-large' 
-accept = 'application/json'
-contentType = 'application/json'
+body = json.dumps({"inputText": llm_prompt})
+modelId = "amazon.titan-tg1-large"
+accept = "application/json"
+contentType = "application/json"
 
-body = json.dumps({'inputText': llm_prompt})
-modelId = 'amazon.titan-tg1-large' 
-accept = 'application/json
-contentType = 'application/json'
-response = bedrock.invoke_model(body=body, modelId=modelId, accept=accept, contentType=contentType)
-response_body = json.loads(response.get('body').read())
-print ('Question: ' + QUESTION_FOR_MODEL)
-print ('Answer: ' + response_body.get('results')[0].get('outputText'))
-    }
-  ],
-  "metadata": {
-    "kernelspec": {
-      "display_name": "Python 3 (ipykernel)",
-      "language": "python",
-      "name": "python3"
-    },
-    "language_info": {
-      "codemirror_mode": {
-        "name": "ipython",
-        "version": 3
-      },
-      "file_extension": ".py",
-      "mimetype": "text/x-python",
-      "name": "python",
-      "nbconvert_exporter": "python",
-      "pygments_lexer": "ipython3"
-    }
-  },
-  "nbformat": 4,
-  "nbformat_minor": 5
-}
+body = json.dumps({"inputText": llm_prompt})
+modelId = "amazon.titan-tg1-large"
+accept = "application/json"
+contentType = "application/json"
+response = bedrock.invoke_model(
+    body=body, modelId=modelId, accept=accept, contentType=contentType
+)
+response_body = json.loads(response.get("body").read())
+print("Question: " + QUESTION_FOR_MODEL)
+print("Answer: " + response_body.get("results")[0].get("outputText"))
